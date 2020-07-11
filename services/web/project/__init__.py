18d1bbc24b056f25e815fb10f0474a967ebc4803
Patrick Snape <patricksnape@gmail.com>
2020-07-11 15:34:57 +0200

    WIP: Add the initial templating
    This adds the initial version of the templating including a load
    if CSS and Javascript that needs to be deleted once I work out
    which page actually needs which scripts/css. At the moment every page
    will include all the CSS and javascript which isn't ideal.
    
    I also added flask-menu to the project and used it to render
    the nav bar with an example of a nested page config.
    
    Now I should be able to deconstruct the base template to create the
    login flow. The current index.html makes no sense but it is just
    a nice example of how to build the sections.

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,7 +1,9 @@
 from flask import (
     Flask,
     render_template_string,
+    render_template,
 )
+from flask_menu import Menu, register_menu
 from flask_sqlalchemy import SQLAlchemy
 from flask_user import UserManager, login_required
 
@@ -12,26 +14,30 @@ from project.mail.mailgun import MailGunEmailAdapter
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app, model_class=BaseModel)
+menu = Menu(app)
 user_manager = UserManager(app, db, User)
 user_manager.email_adapter = MailGunEmailAdapter(app)
 
 
 # The Home page is accessible to anyone
 @app.route("/")
-def home_page():
-    return render_template_string(
-        """
-            {% extends "flask_user_layout.html" %}
-            {% block content %}
-                <h2>{%trans%}Home page{%endtrans%}</h2>
-                <p><a href={{ url_for('user.register') }}>{%trans%}Register{%endtrans%}</a></p>
-                <p><a href={{ url_for('user.login') }}>{%trans%}Sign in{%endtrans%}</a></p>
-                <p><a href={{ url_for('home_page') }}>{%trans%}Home Page{%endtrans%}</a> (accessible to anyone)</p>
-                <p><a href={{ url_for('member_page') }}>{%trans%}Member Page{%endtrans%}</a> (login_required: member@example.com / password)</p>
-                <p><a href={{ url_for('user.logout') }}>{%trans%}Sign out{%endtrans%}</a></p>
-            {% endblock %}
-            """
-    )
+@register_menu(app, ".home", "Home")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/subpage.html")
+@register_menu(app, ".page", "Page With Subpages")  # Note this is a fake page used only for the hierarchy
+@register_menu(app, ".page.subpage", "Subpage")
+def subpage():
+    return render_template("layout.html")
+
+
+@app.route("/subpage2.html")
+@register_menu(app, ".page", "Page With Subpages")  # Note this is a fake page used only for the hierarchy
+@register_menu(app, ".page.subpage2", "Subpage 2")
+def subpage2():
+    return render_template("layout.html")
 
 
 # The Members page is only accessible to authenticated users
