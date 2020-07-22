17f8ad2793d6cebac16b25453afcb0eca11099e3
Patrick Snape <patricksnape@gmail.com>
2020-07-22 12:09:47 +0200

    Add nice __repr__ implementation to the DB models
    Makes it easier to deal with them when they are printed

--- services/web/project/database/__init__.py
+++ services/web/project/database/__init__.py
@@ -1,3 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import as_declarative
 
-BaseModel = declarative_base()
+
+@as_declarative()
+class BaseModel:
+    def __repr__(self):
+        columns = {i.name: getattr(self, i.name) for i in self.__table__.columns}
+        columns_str = ", ".join(f"{k}={v}" for k, v in columns.items())
+        return f"{self.__class__.__name__}({columns_str})"
