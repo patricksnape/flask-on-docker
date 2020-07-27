82d909ebfbcafc159ba14b497db4949c1b3c03ea
Patrick Snape <patricksnape@gmail.com>
2020-07-27 11:20:55 +0200

    Expose global method to jinja for current time

--- services/web/project/jinja/filters/date.py
+++ services/web/project/jinja/filters/date.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 import calendar
 
 from babel.dates import format_date as babel_format_date
@@ -14,3 +14,8 @@ def format_date(value: date, format: str = "long") -> str:
 @custom_jinja_blueprint.app_template_global()
 def calendar_matrix(value: date) -> str:
     return calendar.monthcalendar(value.year, value.month)
+
+
+@custom_jinja_blueprint.app_template_global()
+def now() -> datetime:
+    return datetime.now()
