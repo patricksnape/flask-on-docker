8098d2446375370b8733e5fedf3a8714453d5ace
Patrick Snape <patricksnape@gmail.com>
2020-07-29 13:09:10 +0200

    Add relationship helper attributes to RSVP changes for guest objects
    Had to use some fancy sqlalchemy features to get this done.
    Basically do a subquery join on the unnested array values which
    is pretty inefficient but this table should be pretty small. Wouldn't
    work if it was really millions of entries but it will be in the
    order of 1000 max
    
    Note that accessing these attributes is a bit expensive as
    I cannot get them to pre-load at the moment

--- services/web/project/database/rsvp.py
+++ services/web/project/database/rsvp.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Optional, TYPE_CHECKING
 
-from sqlalchemy import Boolean, Column, Date, DateTime, ForeignKey, Integer, func
+from sqlalchemy import Boolean, Column, Date, DateTime, ForeignKey, Integer, and_, func, select
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.orm import relationship
 
@@ -24,6 +24,8 @@ class RSVPChange(BaseModel):
     id = Column(Integer, primary_key=True)
     party_id = Column(Integer, ForeignKey("party.id"), index=True, nullable=False)
     created_at = Column(DateTime, default=func.now(), nullable=False)
+    # Note using an Array to store foreign keys is a naughty anti-pattern to avoid creating yet another table to store
+    # the changes per user
     guest_ids_before = Column(ARRAY(Integer), nullable=True)
     guest_ids_after = Column(ARRAY(Integer), nullable=True)
     check_in_before = Column(Date, nullable=True)
@@ -38,6 +40,39 @@ class RSVPChange(BaseModel):
     booking = relationship(
         "Booking", secondary="party", primaryjoin=Party.id == party_id, secondaryjoin=Booking.party_id == Party.id,
     )
+
+    __before_guests_selectable = select(
+        [func.unnest(guest_ids_before).label("guest_id"), id.label("rsvp_change_id")]
+    ).alias("guest_ids")
+
+    guests_before = relationship(
+        Guest,
+        secondary=__before_guests_selectable,
+        primaryjoin=and_(
+            Guest.id == __before_guests_selectable.c.guest_id, __before_guests_selectable.c.rsvp_change_id == id
+        ),
+        secondaryjoin=__before_guests_selectable.c.rsvp_change_id == id,
+        order_by=__before_guests_selectable.c.rsvp_change_id,
+        uselist=True,
+        viewonly=True,
+    )
+
+    __after_guests_selectable = select(
+        [func.unnest(guest_ids_after).label("guest_id"), id.label("rsvp_change_id")]
+    ).alias("guest_ids")
+
+    guests_after = relationship(
+        Guest,
+        secondary=__after_guests_selectable,
+        primaryjoin=and_(
+            Guest.id == __after_guests_selectable.c.guest_id, __after_guests_selectable.c.rsvp_change_id == id
+        ),
+        secondaryjoin=__after_guests_selectable.c.rsvp_change_id == id,
+        order_by=__after_guests_selectable.c.rsvp_change_id,
+        uselist=True,
+        viewonly=True,
+    )
+
 
     @property
     def rsvp_declined(self):
