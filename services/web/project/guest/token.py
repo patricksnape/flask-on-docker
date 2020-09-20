fccd330024f8455d497fd91f578d6dc1b3ff9d55
Patrick Snape <patricksnape@gmail.com>
2020-09-20 15:14:57 +0200

    Improve the token code to also disallowed underscores

--- services/web/project/guest/token.py
+++ services/web/project/guest/token.py
@@ -1,13 +1,26 @@
 import secrets
 
 
+def _invalid_token(token: str) -> bool:
+    """
+    A token is invalid if it contains a dash or an underscore.
+
+    Args:
+        token : Token to validate
+
+    Returns:
+        True if the token is invalid (e.g. it contains a '-' or '_')
+    """
+    return "-" in token or "_" in token
+
+
 def get_token() -> str:
     """
     Generates a 6 character random string containing only upper case
     Alphanumeric characters.
     """
     token = secrets.token_urlsafe(4)
-    while "-" in token:
+    while _invalid_token(token):
         token = secrets.token_urlsafe(4)
 
     assert len(token) == 6
